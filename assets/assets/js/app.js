c7c7ce74ffa3fcd975331a76ce671f21979d90c4
T3d <TeddyBear06@users.noreply.github.com>
2021-04-12 01:13:17 +0200

    Update app.js

--- assets/assets/js/app.js
+++ assets/assets/js/app.js
@@ -2,7 +2,20 @@ $('#moteurRecherche').select2({
     placeholder: "Rechercher une thématique ou une leçon...",
     ajax: {
         url: 'https://search.paysdufle.fr/search.php',
-        dataType: 'json'
+        dataType: 'json',
+        processResults: function(data) 
+        {
+            return 
+            {
+                results: $.map(data, function(obj) {
+                    return {
+                        id: obj.id,
+                        text: obj.text,
+                        url: obj.url,
+                    };
+                })
+            };
+        },
     }
 });
 
