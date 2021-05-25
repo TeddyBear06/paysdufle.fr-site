b6ad60b266aa555bc04c3a29fa372fa8aa1e13a3
T3d <TeddyBear06@users.noreply.github.com>
2021-05-25 20:41:22 +0200

    Update app.js

--- assets/assets/js/app.js
+++ assets/assets/js/app.js
@@ -1,38 +1,40 @@
-$('#moteurRecherche').select2({
-    language: "fr",
-    minimumInputLength: 2,
-    placeholder: "Rechercher une thématique ou une leçon...",
-    width: '100%',
-    ajax: {
-        url: meilisearch_domain,
-        dataType: 'json'
-    }
-});
-
-$('#moteurRecherche').on('select2:selecting', function(e) {
-    window.location = e.params.args.data.url;
-});
-
-$('.le_truc_a_modifier').each(function () {
-    var truc = $(this).text().split('@');
-    var machin = truc[1].split('.');
-    $(this).text(machin[0] + ' @ ' + truc[0] + '.com (pensez à retirer les espaces avant et après le symbole @)');
-});
-
-$('.traduirePage').on('click', function() {
-    if(confirm("Vous allez accéder à la traduction automatique de Google. Elle peut contenir des erreurs et certaines imprécisions. Voulez-vous continuer ?")) {
-        var url_page = window.location.href;
-        var url_gg_translate = 'https://translate.google.com/translate?hl=en&sl=fr&tl=en&u=';
-        var url = url_gg_translate + url_page;
-        var win = window.open(url, '_blank');
-        win.focus();
-    }
-    return false;
-});
-
-$("a[href='#suivez-moi-surprise']").on('click', function() {
-    $.getJSON('/assets/json/liste_lecons.json', function(liste_lecons) {
-        var leconSelectionne = liste_lecons[Math.floor(Math.random()*liste_lecons.length)];
-        window.location.href = leconSelectionne.url;
+$(document).ready(function () {
+    $('#moteurRecherche').select2({
+        language: "fr",
+        minimumInputLength: 2,
+        placeholder: "Rechercher une thématique ou une leçon...",
+        width: '100%',
+        ajax: {
+            url: meilisearch_domain,
+            dataType: 'json'
+        }
+    });
+    
+    $('#moteurRecherche').on('select2:selecting', function(e) {
+        window.location = e.params.args.data.url;
+    });
+    
+    $('.le_truc_a_modifier').each(function () {
+        var truc = $(this).text().split('@');
+        var machin = truc[1].split('.');
+        $(this).text(machin[0] + ' @ ' + truc[0] + '.com (pensez à retirer les espaces avant et après le symbole @)');
+    });
+    
+    $('.traduirePage').on('click', function() {
+        if(confirm("Vous allez accéder à la traduction automatique de Google. Elle peut contenir des erreurs et certaines imprécisions. Voulez-vous continuer ?")) {
+            var url_page = window.location.href;
+            var url_gg_translate = 'https://translate.google.com/translate?hl=en&sl=fr&tl=en&u=';
+            var url = url_gg_translate + url_page;
+            var win = window.open(url, '_blank');
+            win.focus();
+        }
+        return false;
+    });
+    
+    $("a[href='#suivez-moi-surprise']").on('click', function() {
+        $.getJSON('/assets/json/liste_lecons.json', function(liste_lecons) {
+            var leconSelectionne = liste_lecons[Math.floor(Math.random()*liste_lecons.length)];
+            window.location.href = leconSelectionne.url;
+        });
     });
 });
